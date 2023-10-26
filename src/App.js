27b09bd52627fa3e05d40e4c719ace523d37aeb8
arewageek <arewageek@gmail.com>
2023-10-26 05:16:13 +0100

    Minor bug fix

--- src/App.js
+++ src/App.js
@@ -308,7 +308,7 @@ function App() {
                           <td className='px-5 py-4'>
                             <div className='flex items-center'>
                               {
-                                token.fromAddress === address ? (
+                                token.fromAddress == address ? (
                                   <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" className="bi bi-arrow-up-right text-red-600" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
                                   </svg>
